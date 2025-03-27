e39f075758e7568755cc2cdb0558b1262a9838fb
Maximiliano Frega <maxifregawebdev@gmail.com>
2025-03-27 13:32:22 -0300

    segunda implementacion de notFound completa

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const NotFound = () => {
+  const handleReload = () => window.location.href = '/'
+
   return (
-    <div>La direccion ingresada no existe</div>
+    <div className='notFoundPageContainer'>
+      <div className='notFoundPageInfo'>
+        <h3>Ups! Parece que la direccion ingresada no existe.</h3>
+        <Link onClick={handleReload}>Volver al inicio</Link>
+      </div>
+    </div>
   )
 }
 
