9b205a420d2c30ccc62ed0613237a384824b1c91
Maximiliano Frega <maxifregawebdev@gmail.com>
2025-05-26 16:50:23 -0300

    responsividad corregida para el menu de categorias, más optimizaciones en el menu

---
+++ src/components/Menu/Categories.js
@@ -0,0 +1,90 @@
+import React, {useState,useEffect,useRef,useLayoutEffect} from 'react'
+import { useSelector, useDispatch } from 'react-redux';
+
+import { currentCategories, setFilterInfo } from '../../redux/searchBarSlice';
+import { getCategoriesList } from '../../auxiliaries/axiosHandlers';
+
+const Categories = ({setHiddenCats, activeCat, onCategorySelected}) => {
+    const dispatch = useDispatch();
+    const currentCats = useSelector(currentCategories);
+    const measureRef = useRef(null);
+    const ulRef = useRef(null);
+
+    const [catWidths, setCatWidths] = useState([]);
+    const [visibleCats, setVisibleCats] = useState(currentCats);
+
+      useEffect(() => {
+        const getCategories = async () => {
+          const filterInfo = await getCategoriesList();
+          if (currentCats.length === 0) dispatch(setFilterInfo(filterInfo));
+        };
+        getCategories();
+        setVisibleCats(currentCats);
+        console.log(currentCats)
+      }, [currentCats, dispatch]);
+
+    useLayoutEffect(() => {
+        if (!measureRef.current) return;
+        const lis = Array.from(
+            measureRef.current.querySelectorAll('li.category-item')
+        );
+        const widths = lis.map(li => li.getBoundingClientRect().width);
+        setCatWidths(widths);
+    }, [currentCats]);
+
+
+    useLayoutEffect(() => {
+    if (catWidths.length === 0 || !ulRef.current) return;
+
+    // leemos dinámicamente el gap de tu UL de categorías
+    const style = window.getComputedStyle(ulRef.current);
+    const GAP = parseFloat(style.gap) || 0;
+    const MORE_BTN_WIDTH = 2; // ancho fijo para el botón "más"
+    const FUZZ = 1;
+
+    const calculate = () => {
+        const contWidth = ulRef.current.clientWidth;
+        let acc = 0;
+        let splitIndex = catWidths.length;
+
+        for (let i = 0; i < catWidths.length; i++) {
+        const extraGap = i > 0 ? GAP : 0;
+        if (acc + catWidths[i] + extraGap <= contWidth - MORE_BTN_WIDTH - FUZZ) {
+            acc += catWidths[i] + extraGap;
+        } else {
+            splitIndex = i;
+            break;
+        }
+        }
+
+        setVisibleCats(currentCats.slice(0, splitIndex));
+        setHiddenCats(currentCats.slice(splitIndex));
+    };
+
+    calculate();
+    const ro = new ResizeObserver(calculate);
+    ro.observe(ulRef.current);
+    window.addEventListener('resize', calculate);
+    return () => {
+        ro.disconnect();
+        window.removeEventListener('resize', calculate);
+    };
+    }, [catWidths, currentCats, setHiddenCats]);
+
+  return <>
+        <ul ref={measureRef} style={{position: 'absolute',visibility: 'hidden',height: 0,overflow: 'hidden',whiteSpace: 'nowrap',}}>
+            {currentCats.map(cat => (
+                <li key={cat} className='category-item'>{cat}</li>
+            ))}
+        </ul>
+        <ul className='categories' ref={ulRef}>
+            {visibleCats.map(cat => (
+                <li key={cat} className={`category-item ${activeCat === cat ? 'activeCategory' : ''}`} onClick={() => dispatch(onCategorySelected(cat))}>
+                    {cat}
+                </li>
+            ))}
+        </ul>
+    </>
+}
+
+export default Categories
--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,51 +1,76 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { BsCart2 } from "react-icons/bs";
+import { MdDoubleArrow } from "react-icons/md";
+import Popover from '@mui/material/Popover';
 
 import Links from '../reusables/Links';
+import { sessionBtns } from '../../data/iconsArray';
 
 import { onCategorySelected, activeCategory } from '../../redux/ProductsSlice';
 import { totalProducts } from '../../redux/CartSlice';
-import { currentCategories, setFilterInfo } from '../../redux/searchBarSlice';
 
-import { sessionBtns } from '../../data/iconsArray';
-import { getCategoriesList } from '../../auxiliaries/axiosHandlers';
+import Categories from './Categories';
 
-const Menu = ({showCats, authSt}) => {
+const Menu = ({ showCats, authSt }) => {
   const dispatch = useDispatch();
-  const activeCat = useSelector(activeCategory)
-  let prodsInCart = useSelector(totalProducts)
-  const currentCats = useSelector(currentCategories)
-  
+  const activeCat = useSelector(activeCategory);
+  const prodsInCart = useSelector(totalProducts);
 
-  useEffect(()=>{
-    console.log(currentCats)
-    const getCategories = async () => {
-      const filterInfo = await getCategoriesList()
-      if(currentCats.length === 0) {
-        dispatch(setFilterInfo(filterInfo))
-      }
+  const [hiddenCats, setHiddenCats] = useState([]);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const isPopoverOpen = Boolean(anchorEl);
+  const popoverId = isPopoverOpen ? 'hiddenCatsPopover' : undefined;
+
+  const openPopover = event => setAnchorEl(event.currentTarget);
+  const closePopover = () => setAnchorEl(null);
+
+  useEffect(() => {
+    if (hiddenCats.length === 0 && isPopoverOpen) {
+      closePopover();
     }
-    getCategories();
-  },[currentCats,dispatch])
-
+  }, [hiddenCats, isPopoverOpen]);
 
   return (
-    showCats === '/' && <div className='menu' style={{justifyContent: showCats === '/cart'?'flex-end':null}}>
-      {/* {showCats==='/' && */}
-        <ul className='categories'>
-          {currentCats.map((cat, index) => {
-            return <li className={`${ activeCat === cat ? 'activeCategory' : ''}`} key={index} onClick={() => dispatch(onCategorySelected(cat))} >{cat}</li>
-          })}
-        </ul>
-      {/* // } */}
+    showCats === '/' && (
+      <div className='menu' style={{ justifyContent: showCats === '/cart' ? 'flex-end' : undefined }}>
+        <Categories setHiddenCats={setHiddenCats} activeCat={activeCat} onCategorySelected={onCategorySelected}/>
         <div className='rightSide'>
-          {authSt && <Links linkArray={sessionBtns}/>}
-          <Link to='/cart'><BsCart2/>{prodsInCart}</Link>
+          {hiddenCats.length > 0 && (
+            <>
+              <button className='more-btn' onClick={openPopover} title='Otras categorías'>
+                <MdDoubleArrow />
+              </button>
+              <Popover id={popoverId} open={isPopoverOpen} onClose={closePopover}
+                anchorEl={anchorEl} disableScrollLock
+                anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+                transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+                slotProps={{paper: {className:'popoverCats'},}}
+              >
+                <p style={{margin: 0, alignSelf: 'center', marginBottom: '4px'}}>Otras categorias</p>
+                {hiddenCats.map(cat => (
+                  <li key={cat} className={`popoverCategory ${activeCat === cat ? 'activeCategory' : ''}`}
+                    onClick={() => {
+                      dispatch(onCategorySelected(cat));
+                      closePopover();
+                    }}
+                  >
+                    {cat}
+                  </li>
+                ))}
+              </Popover>
+            </>
+          )}
+          {authSt && <Links linkArray={sessionBtns} />}
+          <Link to='/cart' title='Carrito' style={{ display: 'flex', alignItems: 'center', gap: '3px' }}>
+            <BsCart2 />
+            {prodsInCart}
+          </Link>
         </div>
-    </div>
-  )
-}
+      </div>
+    )
+  );
+};
 
-export default Menu
+export default Menu;
