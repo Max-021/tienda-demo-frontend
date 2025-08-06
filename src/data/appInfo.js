a134e7e0f90d04868865a803cc67b51c7160b22e
Maximiliano Frega <maxifregawebdev@gmail.com>
2025-08-06 18:43:30 -0300

    cambios en public

--- src/data/appInfo.js
+++ src/data/appInfo.js
@@ -2,7 +2,7 @@ export default {
     lang: 'es',
     title: 'Fly Shop',
     description: 'Aplicación web para venta de artículos de pesca.',
-    favicon: '/assets/icons/fish-svgrepo-com.svg',
-    appleTouchIcon: '/assets/icons/fish-svgrepo-com.svg',
+    favicon: '/assets/icons/logo_fish.svg',
+    appleTouchIcon: '/assets/logo_192.png',
     logo: '',
 }
